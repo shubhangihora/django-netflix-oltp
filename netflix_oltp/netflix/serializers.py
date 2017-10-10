d99ea461ff74b85576ff10c18ebfabce29d92d6f
shubhangihora <shubsy9@gmail.com>
2017-10-10 14:21:04 +0530

    edited serializers.py
    replaced field names with "__all__"

--- netflix_oltp/netflix/serializers.py
+++ netflix_oltp/netflix/serializers.py
@@ -5,17 +5,17 @@ from .models import Content, Access, UserPlan, UserPayment, Director, Producer, 
 class ShowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Show
-        fields = ('show_id', 'seasons')
+        fields = "__all__"
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ('movie_id')
+        fields = "__all__"
 
 class ContentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Content
-        fields = ('content_id', 'name', 'director_id', 'release_date', 'cast_id', 'ratings_id', 'genre_id', 'producer_id', 'time', 'show_id', 'movie_id')
+        fields = "__all__"
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,7 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
 class AccessSerializer(serializers.ModelSerializer):
     class Meta:
         model = Access
-        fields = ('user_id', 'password', 'username')
+        fields = "__all__"
 
 class UserPlanSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,17 +35,17 @@ class UserPlanSerializer(serializers.ModelSerializer):
 class UserPaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserPayment
-        fields = ('user_id', 'payment_id', 'user_payment_id')
+        fields = "__all__"
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
-        fields = ('genre_id','genre_name', 'show_id', 'movie_id')
+        fields = "__all__"
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = ('payment_id', 'plan_id', 'payment_amount')
+        fields = "__all__"
 
 class PlanSerializer(serializers.ModelSerializer):
     class Meta:
@@ -55,24 +55,24 @@ class PlanSerializer(serializers.ModelSerializer):
 class WatchedSerializer(serializers.ModelSerializer):
     class Meta:
         model = Watched
-        fields = ('watched_num', 'user_id')
+        fields = "__all__"
 
 class CastSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cast
-        fields = ('cast_id', 'cast_name')
+        fields = "__all__"
 
 class RatingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ratings
-        fields = ('ratings_id', 'user_id', 'ratings')
+        fields = "__all__"
 
 class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Director
-        fields = ('director_id', 'director_name', 'director_desc')
+        fields = "__all__"
 
 class ProducerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Producer
-        fields = ('producer_id', 'producer_name', 'producer_desc')
+        fields = "__all__"
